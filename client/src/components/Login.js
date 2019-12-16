17e251bc4b86f8ef35a19c49948f7e8cf1c93133
osogrizz <lhtor@gmail.com>
2019-12-15 20:54:53 -0800

    Props is now passing to login component.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,57 +1,74 @@
 import React, { useState } from "react";
-import axios from 'axios'
-import history from '../utils/history'
+// import axios from 'axios'
+import useForm from 'react-hook-form'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 
 const Login = (props) => {
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
-  })
+  console.log(props)
+  const { register, handleSubmit, watch, errors } = useForm()
+
+
+  const onSubmit = data => { 
+    console.log(data)
+    axiosWithAuth().post('http://localhost:5000/api/login', data)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload)
+      props.history.push('/colors')
+    })
+   }
+
+  // const [credentials, setCredentials] = useState({
+  //   username: '',
+  //   password: ''
+  // })
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const handleChange = (e) => {
-    setCredentials({
-      ...credentials,
-        [e.target.name]: e.target.value
-    })
+  // const handleChange = (e) => {
+  //   setCredentials({
+  //     ...credentials,
+  //       [e.target.name]: e.target.value
+  //   })
 
-  }
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    axios
-    .post('http://localhost:5000/api/login', credentials)
-    .then( res => {
-      console.log(res.data)
-      localStorage.setItem('token', res.data.payload)
-      this.props.history.push('/login')
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   axios
+  //   .post('http://localhost:5000/api/login', credentials)
+  //   .then( res => {
+  //     console.log(res.data)
+  //     localStorage.setItem('token', res.data.payload)
+  //     this.props.history.push('/login')
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }
 
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input 
             type="text" 
             name="username" 
             placeholder="username" 
-            onChange={handleChange} 
-            value={credentials.username}
-          />
+            // onChange={handleChange} 
+            ref={register}
+            // value={credentials.username}
+            />
 
           <input 
             type="password" 
             name="password" 
             placeholder="password" 
-            onChange={handleChange} 
-            value={credentials.password} 
+            // onChange={handleChange} 
+            ref={register}
+            // value={credentials.password} 
           />
           <button>Log In</button>
         </form>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Router } from 'react-router-dom'
+// import { Router } from 'react-router-dom'
 import ReactDOM from "react-dom";
-import history from './utils/history'
+// import history from './utils/history'
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Router history={history}><App /></Router>, rootElement);
+ReactDOM.render(<App />, rootElement);
