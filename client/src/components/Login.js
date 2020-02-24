e49ffe68055a1efbbb6b88623d31736aae4cf5f4
osogrizz <lhtor@gmail.com>
2020-02-23 18:46:00 -0800

    Adds login form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,23 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div 
+      style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        margin: '0 auto',
+        flexDirection: 'column'
+      }}
+    >
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div >
+        <form action="">
+          <input type="text" name="name" placeholder="name"/>
+          <input type="text" name="password" placeholder="password"/>
+        </form>
+      </div>
+    </div>
   );
 };
 
